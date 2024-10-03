78dd46aa27150a815c9fea1c0453b96aebac28fd
Navindu Kavishka <navindukavishka2001@gmail.com>
2024-10-03 21:53:41 +0530

    time added

--- src/main/java/controller/PlaceOrderFormController.java
+++ src/main/java/controller/PlaceOrderFormController.java
@@ -2,15 +2,20 @@ package controller;
 
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.text.SimpleDateFormat;
+import java.time.LocalTime;
 import java.util.Date;
 import java.util.ResourceBundle;
 import java.util.logging.SimpleFormatter;
@@ -93,6 +98,18 @@ public class PlaceOrderFormController implements Initializable {
         Date date = new Date();
         SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd");
         lblDate.setText(f.format(date));
+
+        Timeline timeline = new Timeline(new KeyFrame(Duration.ZERO, e -> {
+            LocalTime now = LocalTime.now();
+            lblTime.setText(
+                    now.getHour() + " : " + now.getMinute() + " : " + now.getSecond()
+            );
+        }),
+                new KeyFrame(Duration.seconds(1))
+        );
+
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
     }
 
 
