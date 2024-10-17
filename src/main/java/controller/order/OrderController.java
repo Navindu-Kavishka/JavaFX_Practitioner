7ed00699ff5b7514e2f132d7539c83bd8666edba
Navindu Kavishka <navindukavishka2001@gmail.com>
2024-10-17 18:45:55 +0530

    place order done with transactions

--- src/main/java/controller/order/OrderController.java
+++ src/main/java/controller/order/OrderController.java
@@ -13,23 +13,29 @@ public class OrderController {
 
     public Boolean placeOrder(Order order) throws SQLException {
         Connection connection = DBConnection.getInstance().getConnection();
-        PreparedStatement psTm = connection.prepareStatement("INSERT INTO orders VALUE(?,?,?)");
-        psTm.setObject(1,order.getOrderId());
-        psTm.setObject(2,order.getOrderDate());
-        psTm.setObject(3,order.getCustomerId());
-        boolean isOrderAdded = psTm.executeUpdate() > 0;
+        try {
+            connection.setAutoCommit(false);
+            PreparedStatement psTm = connection.prepareStatement("INSERT INTO orders VALUE(?,?,?)");
+            psTm.setObject(1,order.getOrderId());
+            psTm.setObject(2,order.getOrderDate());
+            psTm.setObject(3,order.getCustomerId());
+            boolean isOrderAdded = psTm.executeUpdate() > 0;
 
-        if (isOrderAdded){
-            boolean isOrderDetailAdded = OrderDetailController.addOrderDetail(order.getOrderDetails());
+            if (isOrderAdded){
+                boolean isOrderDetailAdded = OrderDetailController.addOrderDetail(order.getOrderDetails());
 
-            if (isOrderDetailAdded){
-                boolean isUpdateStock = new ItemController().updateStock(order.getOrderDetails());
+                if (isOrderDetailAdded){
+                    boolean isUpdateStock = new ItemController().updateStock(order.getOrderDetails());
 
-                if (isUpdateStock){
-                    return true;
+                    if (isUpdateStock){
+                        connection.commit();
+                        return true;
+                    }
                 }
             }
+            return true;
+        }finally {
+            connection.setAutoCommit(true);
         }
-        return true;
     }
 }
